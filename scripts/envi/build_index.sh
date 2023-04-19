036d511d0a7fb83a00fd155ac70982f0072bf3ce
nqchieutb01 <63398716+nqchieutb01@users.noreply.github.com>
2023-04-19 09:06:30 +0700

    Update build_index.sh

--- scripts/envi/build_index.sh
+++ scripts/envi/build_index.sh
@@ -3,11 +3,11 @@ set -e
 ckpt_prefix=${MTPATH}/mt.ckpts
 
 
-dataset=${MTPATH}/esen
-ckpt_folder=esen/ckpt.exp.pretrain/epoch19_batch99999_acc0.99
+dataset=${MTPATH}/envi
+ckpt_folder=envi/ckpt.exp.pretrain/epoch19_batch99999_acc0.99
 echo ${ckpt_prefix}/${ckpt_folder}
 python3 build_index.py \
-        --input_file ${dataset}/train.tgt.txt \
+        --input_file ${dataset}/train_rmd.vi \
         --ckpt_path ${ckpt_prefix}/${ckpt_folder}/response_encoder \
         --args_path ${ckpt_prefix}/${ckpt_folder}/args \
         --vocab_path ${dataset}/tgt.vocab \
@@ -15,7 +15,7 @@ python3 build_index.py \
         --batch_size 8192
 
 python3 build_index.py \
-        --input_file ${dataset}/train.tgt.txt \
+        --input_file ${dataset}/train_rmd.vi \
         --ckpt_path ${ckpt_prefix}/${ckpt_folder}/response_encoder \
         --args_path ${ckpt_prefix}/${ckpt_folder}/args \
         --vocab_path ${dataset}/tgt.vocab \
@@ -23,4 +23,4 @@ python3 build_index.py \
         --batch_size 8192 \
         --only_dump_feat
 
-cp ${dataset}/train.tgt.txt ${ckpt_prefix}/${ckpt_folder}/candidates.txt
+cp ${dataset}/train_rmd.vi ${ckpt_prefix}/${ckpt_folder}/candidates.txt
